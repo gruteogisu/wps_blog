1c2809c9ad5e17dd409253804be0c35c932c9b47
gruteogisu <gruteogisu@gmail.com>
2016-06-10 16:26:33 +0900

    refactor models.py: seperate post, naver_post

---
+++ wpsblog/wpsblog/models/post.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.core.urlresolvers import reverse
+
+
+class Post(models.Model):
+    title = models.CharField(
+        max_length=120,
+    )
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse(
+            "posts:detail",
+            kwargs={
+                "post_id": self.id,
+            }
+        )
+
+    def get_update_url(self):
+        return reverse(
+            "posts:update",
+            kwargs={
+                "post_id": self.id,
+            }
+        )
