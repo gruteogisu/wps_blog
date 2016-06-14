17a92ba82bebd944f7fb623b64aefda4e2db3673
gruteogisu <gruteogisu@gmail.com>
2016-06-14 16:05:27 +0900

    init Bitlink model

---
+++ wpsblog/bitly/models/bitlink.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Bitlink(models.Model):
+
+    user = models.ForeignKey(User)
+
+    original_url = models.URLField()
+    shorten_hash = models.CharField(
+        max_length=8,
+        blank=True,
+        null=True,
+    )
+
+    # timestamp
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.original_url
